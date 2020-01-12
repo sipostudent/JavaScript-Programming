6aa73250f0fae59dcf97097331fdc6dd871d8da3
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 21:41:28 +0000

    Functions - Undefined and Null

---
+++ functions/undefined-null.js
@@ -0,0 +1,31 @@
+// Lesson: Undefined and Null
+
+// Undefined for variable
+
+let name
+
+name = 'Sipo'
+
+if (name === undefined) {
+    console.log('Please provide a name')
+} else {
+    console.log(name)
+}
+
+// Undefined for function arguments
+// Undefined as function return default value
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+// Null as assigned value
+let age = 34
+
+age = null
+
+console.log(age)
+
+console.log(age)
