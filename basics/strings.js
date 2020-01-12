e95b3c9040eee386459ceee01d82353bd7401d16
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 15:48:56 +0000

    strings and variables

---
+++ basics/strings.js
@@ -0,0 +1,22 @@
+// Lesson 2: Strings and Variables
+
+// Create a variable to store the city
+let city = 'Birmingham'
+
+// Create a variable to store the County
+let county = 'West Midlands'
+
+// Create a variable to combine the city and county
+let location = city + ', ' + county
+
+// Print the combined location to the terminal
+console.log(location)
+
+
+// Documentation Links 
+
+// The let statement declares a block scope local variable, optionally initializing it to a value.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let 
+
+// The String global object is a constructor for strings or a sequence of characters.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
