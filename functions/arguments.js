d1d76003696a79ec00482e7979e4f0bdae819242
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 23:00:51 +0000

    Functions - Template Strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -9,16 +9,19 @@ console.log(result)
 
 // Default arguments
 let getScoreText = function (name = 'Anonymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}`
 }
 
 let scoreText = getScoreText(undefined, 99)
 console.log(scoreText)
 
-// Challenge area - Tip Calculator
+// Challenge area
+// A 25% tip on $40 would be $10
 let getTip = function (total, tipPercent = .2) {
-    return total * tipPercent
+    let percent = tipPercent * 100
+    let tip = total * tipPercent
+    return `A ${percent}% tip on $${total} would be $${tip}`
 }
 
-let tip = getTip(40, .25)
+let tip = getTip(60)
 console.log(tip)
