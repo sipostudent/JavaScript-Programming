2d5928e04eb1c9ea839e98176e864cd424408bd9
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 16:53:54 +0000

    temp-conversion

---
+++ basics/temp-conversion.js
@@ -0,0 +1,16 @@
+/*
+Challenge Area
+
+1. Calculate celsius and store in celsius variable
+2. Print that value
+
+3. Calculate kelvin and store in celsius variable
+4. Print that value
+*/
+
+let fahrenheit = 32 // 0c - 273.15k
+let celsius = (fahrenheit -32) * 5/9
+let kelvin = (fahrenheit + 459.67) * 5/9
+
+console.log(celsius)
+console.log(kelvin)
