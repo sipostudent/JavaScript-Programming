b72c9e9436dd432f98a075cc188674a4652453ec
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 15:50:21 +0000

    basics - strings & variables

--- basics/strings.js
+++ basics/strings.js
@@ -11,12 +11,3 @@ let location = city + ', ' + county
 
 // Print the combined location to the terminal
 console.log(location)
-
-
-// Documentation Links 
-
-// The let statement declares a block scope local variable, optionally initializing it to a value.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let 
-
-// The String global object is a constructor for strings or a sequence of characters.
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
