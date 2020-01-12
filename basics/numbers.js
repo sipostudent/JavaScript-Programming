b395af20ee2051748fec560f3778b27e251ef1ec
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-12 16:31:28 +0000

    basics - numbers

---
+++ basics/numbers.js
@@ -0,0 +1,30 @@
+// Lesson 3: Numbers
+
+// Create a number and assign to a variable
+let age = 26
+// Add
+let addResult = age + 1 // 27
+// Subtract
+let subtractResult = age - 26.5 // -0.5
+// Multiply
+let multiplyResult = age * 10 // 260
+// Divide
+let divideResult = age / 2 // 13
+// Parenthesis can be used to make sure one operation happens before another
+let result = (age + 3) * 7 // 203
+
+
+let myAge = 34
+let dogYears = (age + 1) / 7
+console.log(dogYears)
+
+
+//  Challenge Area
+
+studentScore = 18
+maxScore = 20
+percent = (studentScore / maxScore) * 100
+
+console.log(percent)
+
+
