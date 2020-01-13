46437dc0cfa53f934be43caa248fe72346aec83b
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-13 12:37:25 +0000

    Object Basics

---
+++ objects/objects-101.js
@@ -0,0 +1,27 @@
+// Lesson 2: Object Basics
+
+// Creating Objects
+
+let myBook = {
+    title: 'The BFG',
+    author: 'Roald Dahl',
+    pageCount: 199
+}
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'James and the Giant Peach'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// Challenge Area
+
+let aboutMe = {
+    name: 'Sipo',
+    age: 34,
+    location: 'Birmingham, UK'
+}
+
+console.log(`My name is ${aboutMe.name}, age is ${aboutMe.age}, and location is ${aboutMe.location}`)
+aboutMe.age = aboutMe.age + 1
+console.log(`My name is ${aboutMe.name}, age is ${aboutMe.age}, and location is ${aboutMe.location}`)
