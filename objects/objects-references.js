729a16b3642b01902a4707effea49d05d79f03c7
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-13 13:52:09 +0000

    Objects - Object References

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,16 +1,32 @@
 // Lesson 4: Object References
 
-// Both logs print the same exact thing
 let myAccount = {
     name: 'Sipo Charles',
     expenses: 0,
     income: 0
-   }
+}
 
-    let addExpense = function (account, amount) {
+let addExpense = function (account, amount) {
     account.expenses = account.expenses + amount
-    console.log(account)
-   }
-   
-   addExpense(myAccount, 2000)
-   console.log(myAccount)
+}
+
+let addIncome = function (account, income) {
+    account.income = account.income + income
+}
+
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+let getAccountSummary = function (account) {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $£{balance}. $£{account.income} in income. $£{account.expenses} in expenses.`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
