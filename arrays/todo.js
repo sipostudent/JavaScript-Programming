60b832fbf427ed2e9d12bbcef88bd959afa19015
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-28 06:59:53 +0000

    Looping Over Arrays

--- arrays/todo.js
+++ arrays/todo.js
@@ -6,9 +6,9 @@ errands.shift()
 
 console.log(`You have ${errands.length} errands!`)
 
-errands.forEach(function (todo, index) {
+errands.forEach(function (errands, index) {
     const num = index + 1
-    console.log(`${num}. ${todo}`)
+    console.log(`${num}. ${errands}`)
 })
 
 // 1. The first item
