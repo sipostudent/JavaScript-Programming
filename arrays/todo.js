d70a59deb88949a562c8cad6ec65a0d0428c1958
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-28 06:23:13 +0000

    Array Basics

---
+++ arrays/todo.js
@@ -0,0 +1,9 @@
+const errands = ['Grocery shopping', 'Gym time', 'Phone a friend', 'Cook dinner', 'Watch Netflix']
+
+console.log(`You have ${errands.length} errands!`)
+console.log(`Todo: ${errands[0]}`)
+console.log(`Todo: ${errands[errands.length - 2]}`)
+
+// Create an array with five errands
+// You have x errands
+// Print the first and second to last items -> Todo: walk the dog
