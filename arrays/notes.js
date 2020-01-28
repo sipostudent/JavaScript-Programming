72ae8556884a690435e908382f347fbddcedb87b
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-28 06:57:22 +0000

    Looping Over Arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -10,5 +10,10 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 
 notes[2] = 'This is now the new note 3'
 
+notes.forEach(function (item, index) {
+    console.log(index)
+    console.log(item)
+})
+
 console.log(notes.length)
 console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -5,4 +5,11 @@ errands.push('Buy coffee')
 errands.shift()
 
 console.log(`You have ${errands.length} errands!`)
-console.log(errands)
+
+errands.forEach(function (todo, index) {
+    const num = index + 1
+    console.log(`${num}. ${todo}`)
+})
+
+// 1. The first item
+// 2. The second item
