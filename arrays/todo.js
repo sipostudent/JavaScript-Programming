e28b34af0e464ab5e5051ff50628b59d6da1d770
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-28 06:36:02 +0000

    Manipulating Arrays with Methods

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,8 @@
 const errands = ['Grocery shopping', 'Gym time', 'Phone a friend', 'Cook dinner', 'Watch Netflix']
 
-console.log(`You have ${errands.length} errands!`)
-console.log(`Todo: ${errands[0]}`)
-console.log(`Todo: ${errands[errands.length - 2]}`)
+errands.splice(2, 1)
+errands.push('Buy coffee')
+errands.shift()
 
-// Create an array with five errands
-// You have x errands
-// Print the first and second to last items -> Todo: walk the dog
+console.log(`You have ${errands.length} errands!`)
+console.log(errands)
