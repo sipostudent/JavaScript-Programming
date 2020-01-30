7991f19099b5e984abc76c712cc4c267284b7a2a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-01-30 07:51:19 +0000

    Strings and Variables

--- basics/strings.js
+++ basics/strings.js
@@ -2,12 +2,16 @@
 
 // Create a variable to store the city
 let city = 'Birmingham'
+let city2 = 'Helsinki'
 
 // Create a variable to store the County
 let county = 'West Midlands'
+let county2 = 'Helsinki County'
 
 // Create a variable to combine the city and county
 let location = city + ', ' + county
+let location2 = city2 + ', ' + county2
 
 // Print the combined location to the terminal
 console.log(location)
+console.log(location2)
